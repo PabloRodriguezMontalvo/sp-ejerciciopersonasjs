532289bb85b92d45c191a9756dd4c54c0d29bd22
luisgiltajamar <luis.gil@tajamar365.com>
2015-04-14 16:17:41 +0000

    A

---
+++ ListaPersonas/Features/Feature1/Feature1.feature
@@ -0,0 +1,11 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="22cc9e1c-900c-4c7b-927e-cb3367267dc2" description="SharePoint App Feature" featureId="22cc9e1c-900c-4c7b-927e-cb3367267dc2" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="ListaPersonas Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="3799b769-6627-4d8c-b80f-51b21e04d335" />
+    <projectItemReference itemId="9e7200b9-6e39-4513-b615-0440502e81be" />
+    <projectItemReference itemId="e224b74c-e5bb-4151-b0b1-4fbda1a22490" />
+    <projectItemReference itemId="2fb91f6c-27d9-496b-8b55-b0d7968125e1" />
+    <projectItemReference itemId="f9f7a35c-0d8b-4531-adeb-c05a47bc4b23" />
+    <projectItemReference itemId="cb5017c1-980b-486c-8212-d07b1dce65c2" />
+  </projectItems>
+</feature>
---
+++ ListaPersonas/Scripts/App.js
@@ -0,0 +1,131 @@
+﻿'use strict';
+
+var context = SP.ClientContext.get_current();
+var lista;
+
+
+
+// This code runs when the DOM is ready and creates a context object which is needed to use the SharePoint object model
+
+function init() {
+    lista = context.get_web().get_lists().getByTitle("Personas");
+}
+
+function listar() {
+    var personas = lista.getItems(new SP.CamlQuery());
+
+    context.load(personas);
+    context.executeQueryAsync(function () {
+        var html = "<table>";
+
+        var enumeracion = personas.getEnumerator();
+        while (enumeracion.moveNext()) {
+            html += "<tr>";
+            var item = enumeracion.get_current();
+            html += "<td>" +
+                item.get_item("Nombre") + "</td>" +
+                "<td>" +
+                item.get_item("Edad") + "</td>" +
+                "</tr>";
+        }
+        html += "</table>";
+        $("#listado").html(html);
+
+    }, function (sender, args) {
+        alert(args.get_message());
+
+    });
+
+
+}
+function crearPersona() {
+
+    var ici = SP.ListItemCreationInformation();
+    var item = lista.addItem(ici);
+    item.set_item("Nombre", $("#txtNombre").val());
+    item.set_item("Edad", $("#txtEdad").val());
+
+    item.update();
+    context.load(item);
+    context.executeQueryAsync(function () {
+        alert("Opinion creada con exito");
+        listar();
+    },
+        function (sender, args) {
+
+            alert(args.get_message());
+        }
+    );
+
+}
+function CrearPersonaRest() {
+    var url = _spPageContextInfo.webServerRelativeUrl + "/_api/web/lists/getByTitle('Personas')/items";
+    var digest = $("#__REQUESTDIGEST").val();
+    var obj = {
+        Nombre: $("#txtNombre").val(),
+        Edad: $("#txtEdad").val()
+    };
+    var objtxt = JSON.stringify(obj);
+
+    $.ajax(
+        {
+            url: url,
+            data: objtxt,
+            type: 'POST',
+            headers: {
+                'accept': 'application/json;odata=verbose',
+                'content-type': 'application/json',
+                'X-RequestDigest': digest
+
+            },
+            success: function () {
+                alert("Gracias por el voto");
+                listarRest();
+
+            },
+            error: function (err) {
+
+                alert(JSON.stringify(err));
+
+            }
+
+        }
+    );
+}
+function listarRest() {
+
+    var url = _spPageContextInfo.webServerRelativeUrl + "/_api/web/lists/getByTitle('Personas')/items";
+    $.ajax({
+        url: url,
+        type: "GET",
+        headers: { "accept": "application/json;odata=verbose" },
+        success: function (res) {
+            var html = "<table>";
+            $.each(res.d.results, function (i, result) {
+                html += "<tr>";
+               
+                html += "<td>" +
+                   result.Nombre + "</td>" +
+                    "<td>" +
+                   result.Edad + "</td>" +
+                    "</tr>";
+
+            });
+            html += "</table>";
+            $("#votos").html(votos);
+        },
+        error: function (err) {
+            alert(JSON.stringify(err));
+        }
+    });
+
+}
+
+$(document).ready(function () {
+    init();
+    listar();
+    $("#btnAdd").bind("click", crearPersona);
+    $("#btnAddRest").bind("click", CrearPersonaRest);
+});
+
+// This function prepares, loads, and then executes a SharePoint query to get the current users information
